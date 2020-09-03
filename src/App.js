9777f40ab52e642a4c0a548c1f905d778b757596
Habib Hinn <habibissahinn@gmail.com>
2020-09-03 16:30:34 +0300

    Fixes
    Fix eslint Error
    Replace asyncComponent with React Lazy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
 
-const asyncCheckout = asyncComponent(() => {
-  return import("./containers/Checkout/Checkout");
-});
+const asyncCheckout = React.lazy(() =>
+  import("./containers/Checkout/Checkout")
+);
 
-const asyncOrders = asyncComponent(() => {
-  return import("./containers/Orders/Orders");
-});
+const asyncOrders = React.lazy(() => import("./containers/Orders/Orders"));
 
-const asyncAuth = asyncComponent(() => {
-  return import("./containers/Auth/Auth");
-});
+const asyncAuth = React.lazy(() => import("./containers/Auth/Auth"));
 
 function App(props) {
   let routes = (
